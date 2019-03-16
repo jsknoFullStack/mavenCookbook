039a41be3c3075a5846c5f7af9b814eb75452784
jsknoFullStack <jsknofullstack@gmail.com>
2019-03-16 09:48:17 +0100

    Multi module and assembly

---
+++ packaging-projects/executable-jar-project/src/main/java/com/maven/jskno/ExecutableJarApp.java
@@ -0,0 +1,15 @@
+package com.maven.jskno;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class ExecutableJarApp {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println(StringUtils.capitalize("jose"));
+    }
+}
